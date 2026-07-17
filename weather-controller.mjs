b8c2a0f2d118f51a64b0d044d0dc5a19e40f7610
Arthur <aadjamoglian@gmail.com>
2026-07-17 13:02:44 -0700

    Added another get request for 5 day forecast

--- weather-controller.mjs
+++ weather-controller.mjs
@@ -6,32 +6,10 @@ import { body, ExpressValidator, validationResult } from 'express-validator';
 const app = express();
 app.use(express.json())
 
-const API = process.env.API;
+const API_KEY = process.env.API_KEY;
 const PORT = process.env.PORT;
 const RESPONSE_404 = {"Error": "Not found"}
 const RESPONSE_400 = {"Error": "Invalid request"}
-
-async function sendCoordinates(message) {
-    console.log(`Sending message: "${message}"`)
-    const url = `https://api.openweathermap.org/data/4.0/onecall/current?lat=${LAT}&lon=${LONG}&appid=${API}`;
-    try {
-        const response = await fetch(url, {
-            method: "GET",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ message: message }),
-            });
-        if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-        }
-
-        const result = await response.json();
-        console.log(result);
-    } catch (error) {
-        console.error(error.message);
-    }
-}
 
 app.listen(PORT, async () => {
     console.log(`Server listening on port ${PORT}...`);
@@ -39,13 +17,16 @@ app.listen(PORT, async () => {
 
 app.get('/current-weather', asyncHandler ( async(req, res) => {
 
-    const { LAT, LONG } = req.query;
-    console.log("Latitude: " + LAT)
-    console.log("Longtitude: " + LONG)
+    // Grab lat and long from query parameters
+    const { lat, lon } = req.query;
+    console.log("Latitude: " + lat)
+    console.log("Longtitude: " + lon)
 
     try {
-        const url = `https://api.openweathermap.org/data/4.0/onecall/current?lat=${LAT}&lon=${LONG}&appid=${API}`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
         console.log(url)
+
+        // Call Open Weather API for weather given lat and long
         const response = await fetch(url, {
             method: "GET",
             headers: {
@@ -56,19 +37,51 @@ app.get('/current-weather', asyncHandler ( async(req, res) => {
         throw new Error(`Response status: ${response.status}`);
         }
 
+        // Save json response
         const result = await response.json();
         console.log(result);
+
+        // Send result as response
+        res.status(200).type('application/json').send(result)
+
     } catch (error) {
         console.error(error.message);
     }
 
-    }));
+}));
 
-// app.post('/send-message', asyncHandler ( async (req, res, next) => {
 
-//     // Get message from get request
-//     const send_message = req.body.message;
-//     console.log(send_message)
-//     sendMessage()
+app.get('/forecast-weather', asyncHandler ( async(req, res) => {
 
-// }))
+    // Grab lat and long from query parameters
+    const { lat, lon } = req.query;
+    console.log("Latitude: " + lat)
+    console.log("Longtitude: " + lon)
+
+    try {
+        const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=5&appid=${API_KEY}`;
+        console.log(url)
+
+        // Call Open Weather API for weather given lat and long
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            }});
+
+        if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+        }
+
+        // Save json response
+        const result = await response.json();
+        console.log(result);
+
+        // Send result as response
+        res.status(200).type('application/json').send(result)
+
+    } catch (error) {
+        console.error(error.message);
+    }
+
+}));
